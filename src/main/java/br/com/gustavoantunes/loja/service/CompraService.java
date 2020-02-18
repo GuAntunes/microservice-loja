e714056bcdedce04c09969eb009db04324627676
Gustavo Antunes <gas.antunes84@gmail.com>
2020-02-18 18:56:33 -0300

    Adicionado hystrics ao projeto

--- src/main/java/br/com/gustavoantunes/loja/service/CompraService.java
+++ src/main/java/br/com/gustavoantunes/loja/service/CompraService.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import br.com.gustavoantunes.loja.client.FornecedorClient;
 import br.com.gustavoantunes.loja.controller.dto.CompraDTO;
 import br.com.gustavoantunes.loja.controller.dto.InfoFornecedorDTO;
@@ -23,28 +25,40 @@ public class CompraService {
 	@Autowired
 	private FornecedorClient fornecedorClient;
 
+	@HystrixCommand(fallbackMethod = "realizaCompraFallback")
 	public Compra realizaCompra(CompraDTO compra) {
-		
+
 //		ResponseEntity<InfoFornecedorDTO> exchange = client.exchange(
 //				"http://fornecedor/info/" + compra.getEndereco().getEstado(), HttpMethod.GET, null,
 //				InfoFornecedorDTO.class);
-		
+
 		String estado = compra.getEndereco().getEstado();
-		
+
 		LOG.info("Buscando informações do fornecedor de {}", estado);
 		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(estado);
 
 		LOG.info("Realizando um pedido");
 		InfoPedidoDTO pedido = fornecedorClient.realizaPedido(compra.getItens());
-				
+
 		System.out.println(info.getEndereco());
-		
+
 		Compra compraSalva = new Compra();
 		compraSalva.setPedidoId(pedido.getId());
 		compraSalva.setTempoDePreparo(pedido.getTempoDePreparo());
 		compraSalva.setEnderecoDestino(compra.getEndereco().toString());
-
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return compraSalva;
+	}
+
+	public Compra realizaCompraFallback(CompraDTO compra) {
+		Compra compraFallBack = new Compra();
+		compraFallBack.setEnderecoDestino(compra.getEndereco().toString());
+		return compraFallBack;
 	}
 
 }
